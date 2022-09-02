b678d48a6adb0bc19f654ab3a2f0284306b4613c
karimamaaoui <maaouika07@gmail.com>
2022-09-02 20:19:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import AboutMe from './portfolioContainer/aboutMe/AboutMe';
 import Home from './portfolioContainer/home/Home';
+import ResumeMe from './portfolioContainer/resume/ResumeMe';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <AboutMe/>
+      <ResumeMe/>
      </div>
   );
 }
